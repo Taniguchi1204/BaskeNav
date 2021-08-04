811b9bbf514e0d021aa36d0668f2d13d9e88d203
EC2 Default User <ec2-user@ip-172-31-31-178.ap-northeast-1.compute.internal>
2021-08-04 09:11:15 +0000

    setting finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,46 @@
 Rails.application.routes.draw do
+
+  # 施設管理者用アカウント
   devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   registrations: 'admins/registrations'
-}
-devise_for :users, controllers: {
-  sessions:      'users/sessions',
-  registrations: 'users/registrations'
-}
+  }
+
+  # 会員用アカウント
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  # ログイン前のリンク先
   root "homes#top"
   get "/news" => "homes#news"
   get "/game" => "homes#game"
 
+  # 会員（フォロー、フォロワー）
   resources :users, only:[:show, :edit, :update] do
+    resources :relationship, only:[:create, :destroy]
     member do
       get :following, :followers
     end
   end
 
-  resources :relationship, only:[:create, :destroy]
+  # ダイレクトメッセージ
   resources :chats, only:[:show, :create]
+
+  # 投稿
   resources :posts do
     resource :favorite_posts, only:[:create, :destroy]
     resources :post_comments, only:[:create, :destroy]
   end
 
-  resources :facilities, only:[:index, :show] do
+  # バスケ施設（施設管理者側を分ける？）
+  resources :facilities do
     resource :facility_favorites, only:[:create, :destroy]
     resources :facility_comments, only:[:create, :destroy]
   end
 
+  #　バスケ施設予約
   resources :reserve_facilities, only:[:show, :create, :edit, :update, :destroy]
-  
-  resources :post_facilities, only:[:new,:create,:edit,:update,:destroy]
-
-
-
 
 end
