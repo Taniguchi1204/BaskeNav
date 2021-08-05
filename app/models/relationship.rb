e936edda56d5758e92dbc71e3b527e8faa6c7903
EC2 Default User <ec2-user@ip-172-31-31-178.ap-northeast-1.compute.internal>
2021-08-05 07:13:00 +0000

    create showpage

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,9 @@
 class Relationship < ApplicationRecord
   with_options presence: true do
-    validates :following, class_name: "User"
-    validates :followed,  class_name: "User"
+    validates :following
+    validates :followed
   end
+
+  belongs_to :following, class_name: "User"
+  belongs_to :followed,  class_name: "User"
 end
