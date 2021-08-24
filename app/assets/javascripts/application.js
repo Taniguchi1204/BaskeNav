9d5b84668f85ece2be7029e64e4737d18519d4b3
EC2 Default User <ec2-user@ip-172-31-31-178.ap-northeast-1.compute.internal>
2021-08-24 07:28:59 +0000

    create user-form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -144,3 +144,17 @@ $(function () {
       }
     })
   });
+
+  //任意のタブにURLからリンクするための設定
+$(function() {
+  $('.game--years__tab a').on('click', function() {
+    let parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
+			$('.game--years__tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
+			$(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
+	})
+})
+
+// 上記の動きをページが読み込まれたらすぐに動かす
+$(window).on('load', function () {
+  $('.game--years__tab li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
+});
