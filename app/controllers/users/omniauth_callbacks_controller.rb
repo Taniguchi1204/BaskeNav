0c7c7d846c4407f923972537fa1bc6f61caf19ed
Tatsuya1204 <tatsuya9951204@gmail.com>
2021-08-30 21:01:20 +0000

    google account error fixed

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,12 +8,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def callback_for(provider)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
+    if @user.present?
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
-      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
+      flash[:notice] = "googleログイン前に会員登録をしてください"
       redirect_to new_user_registration_url
+
     end
   end
 
